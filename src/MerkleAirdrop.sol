39fffa969b16d95dfe4df918bd0b5c4e8826dbe2
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2024-07-12 18:16:17 +0100

    Added CEI for the claim function to prevent reentrancy attack

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -9,6 +9,7 @@ contract MerkleAirdrop {
     // Errors
     //////////////////////////////////////////////////////////////////////////////////////////
     error  MerkleAirdrop__InvalidProof();
+    error MerkleAirdrop__AlreadyClaimed();
 
     //////////////////////////////////////////////////////////////////////////////////////////
     // Events
@@ -18,6 +19,7 @@ contract MerkleAirdrop {
     address[] claimers;
     bytes32 private immutable i_merkleRoot;
     IERC20 private immutable i_airdropToken;
+    mapping (address claimer => bool claimed ) private s_hasClaimed;
 
     constructor (bytes32 merkleRoot, IERC20 airdropToken){
         i_airdropToken = airdropToken;
@@ -25,10 +27,14 @@ contract MerkleAirdrop {
     }
 
     function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        if (s_hasClaimed[account]){
+            revert MerkleAirdrop__AlreadyClaimed();
+        }
         bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
         if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)){
             revert MerkleAirdrop__InvalidProof();
         }
+        s_hasClaimed[account] = true;
         emit Claim (account, amount);
         i_airdropToken.transfer(account, amount);
     }
